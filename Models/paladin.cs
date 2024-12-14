cc04a009b3a4d51314403b0a317f9f8b875a68e9
MelfStudent <melvin.favaro@ynov.com>
2024-12-14 18:10:12 +0100

    chore: removal of two unused methods Heroic Strike and BattleCry, revised warrior counterattack, and addition of the attacker in the defend method

--- Models/paladin.cs
+++ Models/paladin.cs
@@ -13,13 +13,13 @@ public class Paladin : Character
         AttackReductionNumber = 0;
     }
     
-    protected override DefenseResult Defend(TypeDamage typeOfAttack, int attackPower)
+    protected override DefenseResult Defend(Character attacker, TypeDamage typeOfAttack, int attackPower)
     {
         var result = new DefenseResult();
         
         Console.WriteLine("\n========== DEFENSE PHASE ==========");
         Console.WriteLine($"[{Name.ToUpper()}] is under attack!");
-        base.Defend(typeOfAttack, attackPower);
+        base.Defend(attacker, typeOfAttack, attackPower);
 
         return result;
     }
