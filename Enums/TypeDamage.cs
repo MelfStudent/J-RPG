c57f07278dced0302b0462882c96de094bf3efef
MelfStudent <melvin.favaro@ynov.com>
2024-12-25 01:40:00 +0100

    chore: added code documentation for ActionType, AffectedStat, TargetType, TypeDamage and TypeOfArmor enums

--- Enums/TypeDamage.cs
+++ Enums/TypeDamage.cs
@@ -1,8 +1,23 @@
-﻿namespace J_RPG.Enums;
-
-public enum TypeDamage
+﻿namespace J_RPG.Enums
 {
-    Physical,
-    Magic,
-    Null
+    /// <summary>
+    /// Enum representing the types of damage that can be dealt in the game.
+    /// </summary>
+    public enum TypeDamage
+    {
+        /// <summary>
+        /// Represents physical damage, typically based on the attacker's physical stats and the target's physical defense.
+        /// </summary>
+        Physical,
+
+        /// <summary>
+        /// Represents magic damage, typically based on the attacker's magic stats and the target's magic resistance.
+        /// </summary>
+        Magic,
+
+        /// <summary>
+        /// Represents a type of damage that has no effect, often used for special cases or non-damaging actions.
+        /// </summary>
+        Null
+    }
 }
